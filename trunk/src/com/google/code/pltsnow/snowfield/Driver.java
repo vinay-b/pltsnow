dcb96a79b5218110bb51843db2558c04b4cc2656
wilbal1087 <wilbal1087@0763367c-f8c9-11dd-aa46-671127399dce>
2009-04-15 04:59:09 +0000

    added basic driver class

---
+++ trunk/src/com/google/code/pltsnow/snowfield/Driver.java
@@ -0,0 +1,14 @@
+package com.google.code.pltsnow.snowfield;
+
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		SnowProgramImp prog = new SnowProgramImp();
+		prog.initProgram();
+		prog.startProgram();
+	}
+}
