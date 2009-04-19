d47fe61fe9c4d3a3228b744caca1896a6d6d0ddf
sharadh <sharadh@0763367c-f8c9-11dd-aa46-671127399dce>
2009-04-19 09:47:53 +0000

    Excetion Classes

---
+++ src/com/google/code/pltsnow/exception/UnsupportedOperationException.java
@@ -0,0 +1,8 @@
+package com.google.code.pltsnow.exception;
+
+public class UnsupportedOperationException extends Exception {
+
+	public UnsupportedOperationException(String string_) {
+		super(string_);
+	}
+}
