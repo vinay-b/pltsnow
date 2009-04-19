095ba3a8a9a8b929cdfbfecc3d99cddec0b3adbe
sharadh <sharadh@0763367c-f8c9-11dd-aa46-671127399dce>
2009-04-19 09:47:02 +0000

    Exception Classes Added

---
+++ trunk/src/com/google/code/pltsnow/exception/InvalidArgumentException.java
@@ -0,0 +1,9 @@
+package com.google.code.pltsnow.exception;
+
+public class InvalidArgumentException extends Exception {
+
+	public InvalidArgumentException(String string_) {
+		super(string_);
+	}
+
+}
